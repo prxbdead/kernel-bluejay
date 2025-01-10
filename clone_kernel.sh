b29c0ce17bf58df4676a807ea31be86bfd663b62
prxbdead <roland.gerg.123@gmail.com>
2025-01-10 22:10:52 +0200

    updated url

--- clone_kernel.sh
+++ clone_kernel.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p ./builds/kernel
 cd ./builds/kernel
 
-repo init --depth=1 -u https://gitlab.hentaios.com/hentaios-gs-6.x/manifest -b Vallhound
+repo init --depth=1 -u https://gitlab.hentaios.com/hentaios-gs-6.x/manifest.git/ -b Vallhound
 echo "Repo initialized successfully!"
 repo --version
 echo "Begin repo sync..."
