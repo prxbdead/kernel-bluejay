090b5bbe79756ca3bf36e57bf52cea82a87b895b
prxbdead <roland.gerg.123@gmail.com>
2025-01-10 21:56:22 +0200

    made repo sync verbose for debugging

--- clone_kernel.sh
+++ clone_kernel.sh
@@ -7,4 +7,4 @@ cd ./build/kernel
 
 repo init --depth=1 -u https://gitlab.hentaios.com/hentaios-gs-6.x/manifest -b Vallhound
 repo --version
-repo --trace sync -c -j$(nproc --all) --no-tags --fail-fast
+repo --trace sync -c -j$(nproc --all) --no-tags --fail-fast -v
