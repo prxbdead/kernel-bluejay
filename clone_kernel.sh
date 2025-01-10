6e1a1433a26b832a8a1266b06fb5e138062000ed
prxbdead <roland.gerg.123@gmail.com>
2025-01-10 22:03:25 +0200

    make it always cache kernel

--- clone_kernel.sh
+++ clone_kernel.sh
@@ -6,5 +6,8 @@ mkdir -p ./build/kernel
 cd ./build/kernel
 
 repo init --depth=1 -u https://gitlab.hentaios.com/hentaios-gs-6.x/manifest -b Vallhound
+echo "Repo initialized successfully!"
 repo --version
+echo "Begin repo sync..."
 repo --trace sync -c -j$(nproc --all) --no-tags --fail-fast -v
+echo "Repo sync done!"
