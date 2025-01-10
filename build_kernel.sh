9166e8e1d14b5709fb1351160f8ddfc64854c9f5
prxbdead <roland.gerg.123@gmail.com>
2025-01-10 02:13:24 +0200

    changes

--- build_kernel.sh
+++ build_kernel.sh
@@ -68,7 +68,7 @@ cd ..
 rm -rf ./aosp/android/abi_gki_protected_exports_aarch64
 rm -rf ./aosp/android/abi_gki_protected_exports_x86_64
 
-curl -o ./tools/bazel https://github.com/bazelbuild/bazel/releases/download/8.0.0/bazel-8.0.0-linux-x86_64
+curl -o tools/bazel https://github.com/bazelbuild/bazel/releases/download/8.0.0/bazel-8.0.0-linux-x86_64
 
 exec tools/bazel run \
     --lto=thin \
